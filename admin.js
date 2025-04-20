65500b8e1bf3166049363e7a4e6231961ea2395b
Timoh5709 <153010705+Timoh5709@users.noreply.github.com>
2025-04-20 02:35:13 +0200

    Bug fixing

--- admin.js
+++ admin.js
@@ -36,20 +36,12 @@ const form = document.getElementById("articleForm");
 
 onAuthStateChanged(auth, (user) => {
     if (user) {
-        if (!isUserAuthorized(user)) {
-            alert("Accès refusé : Vous n'êtes pas autorisé à accéder à cette page.");
-            window.location.href = ".";
-        }
+        
     } else {
         alert("Veuillez vous connecter pour accéder à cette page.");
         window.location.href = "login.html";
     }
 });
-
-function isUserAuthorized(user) {
-    const authorizedEmails = ["tpillot27@gmail.com"];
-    return authorizedEmails.includes(user.email);
-}
 
 async function uploadToImgBB(file) {
     const formData = new FormData();
